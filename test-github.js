082fe8f748d1999bf26ac4526b63fab826503597
Abhijeet Soni <abhisonijeet123@gmail.com>
2026-02-09 16:59:03 +0530

    testing for github service

--- test-github.js
+++ test-github.js
@@ -1,8 +1,25 @@
-import { listRepos } from "./mcp-server/services/githubService.js";
+import { createIssue, createRepo, listIssues, listRepos } from "./mcp-server/services/githubService.js";
 
 const test = async () => {
-  const repos = await listRepos();
-  console.log(repos);
+  try {
+    // console.log("Creating repo...");
+
+    // const repo = await createRepo("new-repo-test-1");
+    // console.log("Created:", repo);
+
+    // console.log("\nListing repos...");
+
+    // const repos = await listRepos();
+    // console.log(
+    //   repos.map((r) => r.name)
+    // );
+
+    console.log('listing issue')
+    const issue = await createIssue('AbhiJeetSoni22',"new-rep-1122","new isue")
+    console.log('issue',issue)
+  } catch (err) {
+    console.error("ERROR:", err.response?.data || err.message);
+  }
 };
 
 test();
