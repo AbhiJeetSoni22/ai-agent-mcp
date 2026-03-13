91202b999cdb3b646cced3d632c44ae2292bcd6a
Abhijeet Soni <abhisonijeet123@gmail.com>
2026-03-13 11:08:44 +0530

    test file updated

--- test-github.js
+++ test-github.js
@@ -2,17 +2,7 @@ import { createIssue, createRepo, listIssues, listRepos } from "./mcp-server/ser
 
 const test = async () => {
   try {
-    // console.log("Creating repo...");
-
-    // const repo = await createRepo("new-repo-test-1");
-    // console.log("Created:", repo);
-
-    // console.log("\nListing repos...");
-
-    // const repos = await listRepos();
-    // console.log(
-    //   repos.map((r) => r.name)
-    // );
+ 
 
     console.log('listing issue')
     const issue = await createIssue('AbhiJeetSoni22',"new-rep-1122","new isue")
