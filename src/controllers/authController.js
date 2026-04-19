06fb53b992589b728b39727f437c73f0d6889e26
Abhijeet Soni <abhisonijeet123@gmail.com>
2026-04-20 01:26:34 +0530

    updated redirect url

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -69,7 +69,7 @@ res.cookie("token", token, {
   secure: false, // production me true
 });
 
-res.redirect(process.env.FRONTEND_URL);
+res.redirect(`${process.env.FRONTEND_URL}/chat`);
 
    
   } catch (err) {
