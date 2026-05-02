718478d1a598ed12dd5713e89a5c0942b4f26439
Abhijeet Soni <abhisonijeet123@gmail.com>
2026-05-02 12:31:06 +0530

    logs added

--- src/config/googleOAuth.js
+++ src/config/googleOAuth.js
@@ -1,5 +1,12 @@
+const redirectURI = process.env.GOOGLE_REDIRECT_URI;
+
+if (!redirectURI) {
+  console.error("❌ GOOGLE_REDIRECT_URI is missing");
+  process.exit(1);
+}
+
 export const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_REDIRECT_URI
+  redirectURI
 );
