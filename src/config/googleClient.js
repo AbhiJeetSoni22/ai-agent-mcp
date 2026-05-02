d41735eb89fdb51ddeb974d8364f0dad51296f5a
Abhijeet Soni <abhisonijeet123@gmail.com>
2026-05-02 12:31:34 +0530

    fixes

--- src/config/googleClient.js
+++ src/config/googleClient.js
@@ -1,13 +1,11 @@
-import { google } from "googleapis";
+const redirectURI = process.env.GOOGLE_REDIRECT_URI;
+
+if (!redirectURI) {
+  console.error("❌ GOOGLE_REDIRECT_URI is missing");
+}
 
 const oAuth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_REDIRECT_URI
+  redirectURI
 );
-
-oAuth2Client.setCredentials({
-  refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
-});
-
-export { oAuth2Client };
